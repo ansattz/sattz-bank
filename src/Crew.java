b4739f8b4a8c9c0b509b253a88727caa33854cbe
ansattz <dvieiras@aol.com>
2022-09-01 16:04:26 -0300

    class Crew to test employees objects

---
+++ src/Crew.java
@@ -0,0 +1,21 @@
+
+public class Crew {
+
+	public static void main(String[] args) {
+		Employee henrique = new Employee();
+		Employee ricardo = new Employee();
+		henrique.setSalary(1800);
+		ricardo.setSalary(2500);
+		
+		
+/*		System.out.println("Henrique's salary is "
+				+ henrique.getSalary());
+		
+		System.out.println("Henrique gained a bonus of "
+				+ henrique.salaryBonus());
+		
+		System.out.print("Henrique's salary is ");
+		System.out.println(henrique.salaryBonus() + henrique.getSalary());
+*/
+	}
+}
