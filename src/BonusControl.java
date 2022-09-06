08e1f8cf750883b2dc7501ed9cdb4cff5c5c57ab
ansattz <dvieiras@aol.com>
2022-09-06 14:48:42 -0300

    creating a bonus control class for the employees to overwrite the salaryBonus method from the Employee class

---
+++ src/BonusControl.java
@@ -0,0 +1,18 @@
+
+public class BonusControl {
+   private double bonus;
+
+   public void registry(Employee e) {
+      // I dont know which employee will be passed here
+      // ill just apply the getSalary() on the employee
+      // and he will get the bonus of his kind.
+      // This means that i will alocate every employee in their
+      // correct bonification.
+      this.bonus = this.bonus + e.salaryBonus();
+   }
+
+   public double getBonus() {
+      // call me and get your bonus.
+      return bonus;
+   }
+}
