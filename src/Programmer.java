078f11d0167d3225b3c28e739ee9d535d1aeed43
ansattz <dvieiras@aol.com>
2022-09-07 20:59:44 -0300

    another specific employee that will receive a particular bonification

---
+++ src/Programmer.java
@@ -0,0 +1,7 @@
+public class Programmer extends Employee {
+   // salaryBonus for programmer role gets +150 extra
+   public double salaryBonus() {
+      System.out.println("Programmer received");
+      return super.salaryBonus() + 150;
+   }
+}
