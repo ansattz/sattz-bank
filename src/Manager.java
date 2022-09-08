cd2ad930ed1c3a78ecc4e72f119cd10c86a94d31
ansattz <dvieiras@aol.com>
2022-09-07 21:02:21 -0300

    salaryBonus being applied with super

--- src/Manager.java
+++ src/Manager.java
@@ -1,24 +1,26 @@
-// Manager > Employee
+// Employee greater than Manager 
 public class Manager extends Employee {
-	private int password;
-	
-	public void setPassword(int password) {
-		// setter password
-		this.password = password;
-	}
-	
-	public boolean verify(int password) {
-		// verify if the output of the setter setPassword
-		if(this.password == password) {
-			return true;
-		}else {
-			return false;
-		}
-	}
-	
-	public double salaryBonus() {
-		return super.salaryBonus() + super.getSalary();
-		// super: specifies that the attribute salary
-		// is from the Employee class
-	}
+   private int password;
+
+   public void setPassword(int password) {
+      // setter password
+      this.password = password;
+   }
+
+   public boolean verify(int password) {
+      // bolean method ill verify the output of
+      // the setter setPassword method
+      if (this.password == password) {
+         return true;
+      } else {
+         return false;
+      }
+   }
+
+   public double salaryBonus() {
+      System.out.println("Manager received");
+      return super.salaryBonus() + super.getSalary();
+      // super: specifies that the attribute salary
+      // is from the Employee class
+   }
 }
