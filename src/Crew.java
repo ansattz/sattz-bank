d436f26e0ce383e01902e113082c4c31dfe25a13
ansattz <dvieiras@aol.com>
2022-09-07 21:05:01 -0300

    testing BonusControl class; applying registry to call the specific bonification for this employee -programmer-

--- src/Crew.java
+++ src/Crew.java
@@ -1,26 +1,32 @@
 
 public class Crew {
 
-	public static void main(String[] args) {
-		Employee henrique = new Employee();
-		Employee ricardo = new Employee();
-		henrique.setSalary(1800);
-		ricardo.setSalary(2500);
-		
-		Manager mario = new Manager();
-		mario.setPassword(209040);
-		
-		System.out.println(mario.verify(209042));
-		System.out.println(mario.verify(209040));
+   public static void main(String[] args) {
+      Employee henrique = new Employee();
+      Employee mario = new Manager();
+      Employee douglas = new Programmer();
 
-/*		System.out.println("Henrique's salary is "
-				+ henrique.getSalary());
-		
-		System.out.println("Henrique gained a bonus of "
-				+ henrique.salaryBonus());
-		
-		System.out.print("Henrique's salary is ");
-		System.out.println(henrique.salaryBonus() + henrique.getSalary());
-*/
-	}
+      henrique.setSalary(1800);
+      mario.setSalary(4000);
+      douglas.setSalary(8500);
+
+      BonusControl control = new BonusControl();
+
+      // control.registry(henrique);
+      // control.registry(mario);
+      control.registry(douglas);
+
+      System.out.println(control.getBonus());
+
+      /*
+       * System.out.println("Henrique's salary is "
+       * + henrique.getSalary());
+       * 
+       * System.out.println("Henrique gained a bonus of "
+       * + henrique.salaryBonus());
+       * 
+       * System.out.print("Henrique's salary is ");
+       * System.out.println(henrique.salaryBonus() + henrique.getSalary());
+       */
+   }
 }
