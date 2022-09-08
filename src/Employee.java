d13e72326aab4fc5c4357d6f808d4199081535b1
ansattz <dvieiras@aol.com>
2022-09-07 21:03:29 -0300

    working with protected attribute: salary

--- src/Employee.java
+++ src/Employee.java
@@ -1,31 +1,36 @@
 
 public class Employee {
-	private String name;
-	private String cpf;
-	protected double salary;
-	// public for child class
-	// only child class of Employee can access this as public
-	
-	public double salaryBonus() {
-		return this.salary * 0.1; 
-	}
-	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getCpf() {
-		return cpf;
-	}
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-	public double getSalary() {
-		return salary;
-	}
-	public void setSalary(double salary) {
-		this.salary = salary;
-	}
+   private String name;
+   private String cpf;
+   protected double salary;
+   // public for child class
+   // only child class of Employee can access this as public
+
+   public double salaryBonus() {
+      return this.salary * 0.1;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public String getCpf() {
+      return cpf;
+   }
+
+   public void setCpf(String cpf) {
+      this.cpf = cpf;
+   }
+
+   public double getSalary() {
+      return salary;
+   }
+
+   public void setSalary(double salary) {
+      this.salary = salary;
+   }
 }
